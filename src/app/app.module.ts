649f1a63e768aa2edd7be172c105e163513ee7c5
Arinouchkine <arinouchkine@gmail.com>
2019-05-06 11:52:05 +0200

    final flash

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { registerLocaleData } from '@angular/common';
+import localeFr from '@angular/common/locales/fr';
+registerLocaleData(localeFr, 'fr');
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { CityWeatherComponent } from './city-weather/city-weather.component';
 import { DetailsComponent } from './details/details.component';
+import { ImageTeaserComponent } from './image-teaser/image-teaser.component';
 
 
 @NgModule({
@@ -13,13 +18,17 @@ import { DetailsComponent } from './details/details.component';
     AppComponent,
     HomeComponent,
     CityWeatherComponent,
-    DetailsComponent
+    DetailsComponent,
+    ImageTeaserComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule,
   ],
-  providers: [],
+  providers: [
+    {provide: localeFr, useValue: 'fr-FR'}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {
--- src/app/city-weather/city-weather.component.ts
+++ src/app/city-weather/city-weather.component.ts
@@ -2,24 +2,37 @@ import { Component, OnInit, Input } from '@angular/core';
 import { Router } from '@angular/router';
 import { WeatherService } from '../weather.service';
 
+import { ImageTeaserComponent } from '../image-teaser/image-teaser.component';
+
 @Component({
   selector: 'city-weather',
   templateUrl: './city-weather.component.html',
   styleUrls: ['./city-weather.component.css']
 })
 export class CityWeatherComponent implements OnInit {
+  @Input()
   condition: string;
   currentTemp: number;
   maxTemp: number;
   minTemp: number;
   name: string;
 
-  constructor(public weatherService: WeatherService) {
+  constructor(public weatherService: WeatherService, private router: Router) {
     this.name = 'Paris';
   }
 
   ngOnInit() {
     this.loadWeather();
+  }
+
+  changeName(name)
+  {
+    this.name = name;
+    this.loadWeather();
+  }
+
+  goToDetail(name){
+    this.router.navigateByUrl('/details/' + name);
   }
 
   loadWeather() {
